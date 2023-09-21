e2b2cebb88ada856426873b878ba9fd2cc3892dc
amoljagtap17 <amoljagtap17@gmail.com>
2023-09-21 13:52:01 +0530

    express-mircoservices: add kubernetes and skaffold yamls

--- products-app/src/globals/utils/mongo-connect.ts
+++ products-app/src/globals/utils/mongo-connect.ts
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 export const mongoDBConnect = async () => {
   const MONGO_URI = process.env.MONGO_URI as string;
 
+  console.log("MONGO_URI::", MONGO_URI);
+
   try {
     await mongoose.connect(MONGO_URI);
 
--- products-app/src/index.ts
+++ products-app/src/index.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import http from "http";
 import { app } from "./app";
+import { mongoDBConnect } from "./globals/utils/mongo-connect";
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const start = async () => {
   const PORT = 3000;
 
   const server = http.createServer(app);
+
+  await mongoDBConnect();
 
   server.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}/`);
